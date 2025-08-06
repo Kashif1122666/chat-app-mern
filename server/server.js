cf5e446e827be84d12fe8f66324bca1e1eed6f1b
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-08-06 16:06:57 +0500

    add socket.io

--- server/server.js
+++ server/server.js
@@ -5,14 +5,40 @@ import http from 'http';
 import { connect } from 'http2';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
+import { Server } from 'socket.io';
 
 // create express app and http server 
-
 const app = express();
 const server  = http.createServer(app);
 
-// middlewares setup 
+// initialize socket.io server 
+export const io = new  Server(server,{
+    cors:{
+        origin:"*"
+    }
+});
 
+// store online users 
+export const userSocketMap = {}; // { userId:socketId }
+
+// Socket.io connection handler 
+io.on("connection" , (socket)=>{
+     const userId = socket.handshake.query.userId;
+     console.log("User Connected", userId); 
+     if(userId) userSocketMap[userId] = socket.id;
+
+    //  Emit online users  to all connected clients 
+    io.emit("getOnlineUsers" , Object.keys(userSocketMap));
+
+    socket.on("disconnect",()=>{
+        console.log("User disconnected",userId);
+        delete userSocketMap[userId]
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    });
+})
+
+// middlewares setup 
 app.use(express.json({limit: '4mb'}));
 app.use(cors());
 
@@ -20,6 +46,7 @@ app.use(cors());
 // Routes setup 
 app.use("/api/status" , (req, res) => res.send("Server is live"));
 app.use("/api/auth",userRouter);
+app.use("/api/messages",messageRouter);
 
 // connect to MongoDB 
 await connectDB();
