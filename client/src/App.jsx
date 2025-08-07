9da880a9b61092eddf391ed1f473759f4e4a90c5
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-08-07 15:17:56 +0500

    add socket.io to the frontend.  add login and signup function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
+import {Toaster}  from "react-hot-toast"
+
 
 const App = () => {
   return (
     <div className='min-h-screen bg-no-repeat bg-[center_20%] bg-cover bg-[url("/src/assets/bgImage.svg")]'>
+       <Toaster/>
       <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
