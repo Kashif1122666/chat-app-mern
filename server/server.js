646d8ace38c8005da5b96fb2d585ea765960b5d3
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-08-12 15:43:02 +0500

    add changes to server to remove bug...

--- server/server.js
+++ server/server.js
@@ -61,5 +61,5 @@ await connectDB();
    server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
  }
 
-//  export server for vercel
+//  export server for vercel....
  export default server;
