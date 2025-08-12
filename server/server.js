221924b8231137b01cfde59c9ab18251de550769
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-08-12 15:15:20 +0500

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -53,7 +53,13 @@ await connectDB();
 // console.log("Connected to:", process.env.MONGODB_URI + "/chat-app");
 
 
-const PORT = process.env.PORT || 5000;
 
 // start server
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+ if (process.env.NODE_ENV !== 'production') {
+     const PORT = process.env.PORT || 5000;
+   server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+ }
+
+//  export server for vercel
+ export default server;
