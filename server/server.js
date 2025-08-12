d2b23ba782d787ef2a7c43ae7cf4646dee73fd7e
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-08-12 15:26:28 +0500

    add changes to server to remove bug

--- server/server.js
+++ server/server.js
@@ -1,42 +1,62 @@
 import express from 'express';
 import "dotenv/config";
-import cors from 'cors';
+import cors  from 'cors';
 import http from 'http';
+import { connect } from 'http2';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
 import { Server } from 'socket.io';
 
+// create express app and http server 
 const app = express();
-const server = http.createServer(app);
+const server  = http.createServer(app);
 
-export const io = new Server(server, {
-  cors: { origin: '*' }
+// initialize socket.io server 
+export const io = new  Server(server,{
+    cors:{
+        origin:"*"
+    }
 });
 
-export const userSocketMap = {};
+// store online users 
+export const userSocketMap = {}; // { userId:socketId }
 
-io.on('connection', (socket) => {
-  const userId = socket.handshake.query.userId;
-  console.log('User Connected', userId);
-  if (userId) userSocketMap[userId] = socket.id;
+// Socket.io connection handler 
+io.on("connection" , (socket)=>{
+     const userId = socket.handshake.query.userId;
+     console.log("User Connected", userId); 
+     if(userId) userSocketMap[userId] = socket.id;
 
-  io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    //  Emit online users  to all connected clients 
+    io.emit("getOnlineUsers" , Object.keys(userSocketMap));
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected', userId);
-    delete userSocketMap[userId];
-    io.emit('getOnlineUsers', Object.keys(userSocketMap));
-  });
-});
+    socket.on("disconnect",()=>{
+        console.log("User disconnected",userId);
+        delete userSocketMap[userId]
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    });
+})
 
-app.use(express.json({ limit: '4mb' }));
+// middlewares setup 
+app.use(express.json({limit: '4mb'}));
 app.use(cors());
 
-app.use('/api/status', (req, res) => res.send('Server is live'));
-app.use('/api/auth', userRouter);
-app.use('/api/messages', messageRouter);
 
+// Routes setup 
+app.use("/api/status" , (req, res) => res.send("Server is live"));
+app.use("/api/auth",userRouter);
+app.use("/api/messages",messageRouter);
+
+// connect to MongoDB 
 await connectDB();
+// console.log("Connected to:", process.env.MONGODB_URI + "/chat-app");
 
-export default server;
+
+
+// start server
+
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
